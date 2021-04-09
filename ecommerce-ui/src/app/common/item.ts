34459e615d80fa664c561608f5b315584c0a0b5f
michaelzheng1 <mzheng1@terpmail.umd.edu>
2021-04-08 21:18:01 -0400

    add ecommerce ui folder

---
+++ ecommerce-ui/src/app/common/item.ts
@@ -0,0 +1,12 @@
+export class Item {
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpated: Date;
+}
+
